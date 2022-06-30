c64cf847f73b17537e7c25e0cdf96a8f17f43844
RohitShelke10509 <rohit216.ht@gmail.com>
2022-06-30 18:11:46 +0530

    cardscript

---
+++ userlist.js
@@ -0,0 +1,27 @@
+var app=angular.module('userList',[]);
+
+app.controller('userListCtrl',function($scope){
+
+    
+        $scope.firstname='Rohit';
+        $scope.lastName='shelke';
+        $scope.email='rohit@gmail.com';
+        $scope.status='active';
+
+
+
+        $scope.firstname1='amar';
+        $scope.lastName1='akabr';
+        $scope.email1='amar@gmail.com';
+        $scope.status1='Inactive';
+
+        $scope.firstname2='HK';
+        $scope.lastName2='BK';
+        $scope.email2='HK@gmail.com';
+        $scope.status2='active';
+
+
+
+
+
+});
